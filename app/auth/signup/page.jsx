f5b443e815a51c382ee40731d14b4e832354ef5b
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-18 00:03:50 +0100

    add validation to the signup, send user data to the api, and redirect it to login page

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -2,28 +2,32 @@
 import { useState } from "react";
 import Link from "next/link";
 import axios from "axios";
-
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 const SignIn = () => {
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [response, setResponse] = useState(null);
+ 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+   const router = useRouter();
+  const onSubmit = async (formData) => {
+    
     try {
       const { data } = await axios.post(
         "http://127.0.0.1:5000/api/users/signup",
-        {
-          name,
-          email,
-          password,
-        },
-        {
-          withCredentials: true,
-        }
+        formData
       );
-      console.log(data);
+      if(data){
+        setResponse(data?.message)
+        setTimeout(() => {
+          router.push("/auth/login");
+        }, 2000);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -40,9 +44,13 @@ const SignIn = () => {
 
       <div className="flex flex-col justify-center px-4 bg-gray-100">
         <form
-          onSubmit={(e) => onSubmit(e)}
-          className="max-w-[400px] w-full mx-auto bg-white px-10 py-6"
+          onSubmit={handleSubmit(onSubmit)}
+          className="max-w-[450px] w-full mx-auto bg-white px-10 py-6"
         >
+          {response && (
+            <div className="block text-center">
+              <p className="font-bold text-green-500">{response}</p>
+            </div>)}
           <h2 className="py-6 text-4xl font-bold text-center">Sign Up</h2>
           <div className="flex flex-col py-2">
             <label>name</label>
@@ -50,9 +58,16 @@ const SignIn = () => {
               className="p-2 border"
               type="text"
               placeholder="name"
-              onChange={(e) => setName(e.target.value)}
-              required
+              // onChange={(e) => setName(e.target.value)}
+              {...register("name", { required: true, minLength: 6 })}
             />
+            {errors.name && (
+              <span className="text-red-500">
+                {errors.name.type === "required"
+                  ? "This field is required"
+                  : "Name must be at least 6 characters"}
+              </span>
+            )}
           </div>
           <div className="flex flex-col py-2">
             <label>Email</label>
@@ -60,9 +75,19 @@ const SignIn = () => {
               className="p-2 border"
               type="text"
               placeholder="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
+              // onChange={(e) => setEmail(e.target.value)}
+              {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
             />
+            {errors.email && (
+              <span className="text-red-500">
+                {errors.email.type === "required"
+                  ? "This field is required"
+                  : "Enter a valid email address"}
+              </span>
+            )}
           </div>
           <div className="flex flex-col py-2">
             <label>Password</label>
@@ -70,17 +95,25 @@ const SignIn = () => {
               className="p-2 border"
               type="password"
               placeholder="password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              minLength={6}
+              // onChange={(e) => setPassword(e.target.value)}
+              {...register("password", { required: true, minLength: 6 })}
             />
+            {errors.password && (
+              <span className="text-red-500">
+                {errors.password.type === "required"
+                  ? "This field is required"
+                  : errors.password.type === "minLength"
+                  ? "password must be at least 6 characters"
+                  : "Invalid Password"}
+              </span>
+            )}
           </div>
           <button className="w-full py-2 my-5 text-white bg-red-500 border hover:bg-red-400">
             register
           </button>
           <div className="flex justify-center gap-2">
             <p className="flex items-center">If you already have an account</p>
-            <Link className="font-bold text-red-600" href="/login">
+            <Link className="font-bold text-red-600" href="/auth/login">
               login
             </Link>
           </div>
