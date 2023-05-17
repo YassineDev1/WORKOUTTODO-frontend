a49eb7149565ecdc007fb4fd245353af009f7a8f
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-18 00:49:01 +0100

    add min-h-screen class

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-full">
+      <body className="min-h-screen">
         <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
