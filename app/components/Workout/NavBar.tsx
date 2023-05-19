8da964191296c5e78b9488948eb21aa25d62a7f7
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-20 00:04:40 +0100

    change some classes in NavBar

--- app/components/Workout/NavBar.tsx
+++ app/components/Workout/NavBar.tsx
@@ -8,10 +8,10 @@ interface NavBar {
 
 const NavBar: React.FC<NavBar> = ({ name }) => {
   return (
-    <nav className="flex items-center justify-between p-4 bg-[#FAFAFA]">
+    <nav className="flex items-center gap-12 justify-between p-4 bg-[#FAFAFA]">
       <div className="text-xl font-bold text-[#1E1E1E]">WORKOUTTODO</div>
-      <div className="text-[#1E1E1E]">
-        <span className="mr-2">Welcome, {name}</span>
+      <div className="text-[#1E1E1E] flex sm:flex-row flex-col justify-center items-center">
+        <span className="text-center text-[#1E1E1E]">Welcome, {name}</span>
         <button
           onClick={() => signOut()}
           className="px-4 py-1 text-white bg-red-500 rounded-md hover:bg-red-400"
