b985cbc77779750d4b2f7f86bd53b58800125410
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-19 21:57:43 +0100

    change sessionProvider to Providers wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
 import "./globals.css";
+import Providers from "./Providers";
 
 export const metadata = {
   title: "WORKOUTTODO",
@@ -9,15 +9,13 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: any;
 }) {
   return (
     <html lang="en">
       <body className="min-h-screen">
-        <SessionProvider session={session}>{children}</SessionProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
