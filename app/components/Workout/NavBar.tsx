5f186e1928e731a2197072b2271938e90bd0f06e
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-20 00:45:54 +0100

    add gap-4 in NavBar

--- app/components/Workout/NavBar.tsx
+++ app/components/Workout/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar: React.FC<NavBar> = ({ name }) => {
   return (
     <nav className="flex items-center gap-12 justify-between p-4 bg-[#FAFAFA]">
       <div className="text-xl font-bold text-[#1E1E1E]">WORKOUTTODO</div>
-      <div className="text-[#1E1E1E] flex sm:flex-row flex-col justify-center items-center">
+      <div className="text-[#1E1E1E] flex sm:flex-row flex-col gap-4 justify-center items-center">
         <span className="text-center text-[#1E1E1E]">Welcome, {name}</span>
         <button
           onClick={() => signOut()}
