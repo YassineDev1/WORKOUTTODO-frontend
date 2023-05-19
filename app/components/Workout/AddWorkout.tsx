e4bbea1b436dd063626399b1e65074d2605e73e4
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-19 23:19:42 +0100

    remove useless code

--- app/components/Workout/AddWorkout.tsx
+++ app/components/Workout/AddWorkout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { WorkoutType } from "@/app/types/Workout";
 import { useForm } from "react-hook-form";
 
@@ -9,34 +8,21 @@ interface AddWorkoutProps {
 }
 
 const AddWorkout: React.FC<AddWorkoutProps> = ({ handleAddedWorkout }) => {
-  const [title, setTitle] = useState("");
-  const [reps, setReps] = useState(0);
-  const [load, setLoad] = useState(0);
-
-   const {
-     register,
-     handleSubmit,
-     formState: { errors },
-   } = useForm()
-
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-  };
-
-  const handleRepsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setReps(Number(event.target.value));
-  };
-
-  const handleLoadChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLoad(Number(event.target.value));
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleAddWorkout = async (formData: any) => {
     await handleAddedWorkout(formData);
   };
 
   return (
-    <form onSubmit={handleSubmit(handleAddWorkout)} className="self-center w-full max-w-xl p-4 mx-auto bg-white shadow-md md:self-start">
+    <form
+      onSubmit={handleSubmit(handleAddWorkout)}
+      className="self-center w-full max-w-xl p-4 mx-auto bg-white shadow-md md:self-start"
+    >
       <h2 className="mb-4 text-xl font-semibold">Add Workout</h2>
       <div className="mb-4">
         <label className="block mb-2" htmlFor="title">
@@ -83,7 +69,10 @@ const AddWorkout: React.FC<AddWorkoutProps> = ({ handleAddedWorkout }) => {
           <span className="text-red-500">This field is required</span>
         )}
       </div>
-      <button type="submit" className="w-full py-2 mt-4 text-white bg-red-500 border hover:bg-red-400">
+      <button
+        type="submit"
+        className="w-full py-2 mt-4 text-white bg-red-500 border hover:bg-red-400"
+      >
         Add Workout
       </button>
     </form>
